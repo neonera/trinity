852aa492812c0b263c01100788482959f905e64e
Everett Flynn <everettflynn25@gmail.com>
2022-08-10 21:40:01 -0700

    Demo Improvements
    Keybinds:
    u - set screen to "user"
    t - set screen to "tv"
    b - bowl completely random pins
    / - create a spare or strike
    i - start interval (the two scores of the last frame bowled placed next to each other and multiplied by 100 is the interval in ms)

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 	import type { Bowlers, Pins } from "./types";
 	import UserScreen from "./UserScreen/UserScreen.svelte";
 
-	let screenType = "tv";
+	let screenType = "user";
 
 	// prettier-ignore
 	let bowlers: Bowlers = {
@@ -19,87 +19,111 @@
 	let currentFrame = 7;
 
 	const bowlPins = (pins_knocked: Pins) => {
+		if (currentFrame === 11) return;
+
+		const pins_amt = pins_knocked.length;
+		let reset_pins = pins_amt === pins.length;
 		pins_knocked.forEach((pin_num) => {
 			const index = pins.indexOf(pin_num);
 			if (index > -1) pins.splice(index, 1);
 		});
 		pins = pins;
 
-		const pins_amt = pins_knocked.length;
 		const frames = bowlers[currentBowler].frames;
 		const next_frame = frames.length ?? 0;
 		const last_frame = next_frame - 1;
-		let reset_pins = false;
+		let next_player = false;
 		if (frames.length === 0) {
-			if (pins_amt === 10) bowlers[currentBowler].frames[0] = [10, 0];
-			else bowlers[currentBowler].frames[0] = [pins_amt];
+			if (pins_amt === 10) {
+				bowlers[currentBowler].frames[0] = [10, 0];
+				next_player = true;
+			} else bowlers[currentBowler].frames[0] = [pins_amt];
 		} else if (frames.length < 10) {
 			if (frames[last_frame].length === 1) {
 				bowlers[currentBowler].frames[last_frame] = [frames[last_frame][0], pins_amt];
-				reset_pins = true;
+				next_player = true;
 			} else if (frames[last_frame].length === 2) {
 				if (pins_amt === 10) {
 					if (frames[8]) bowlers[currentBowler].frames[next_frame] = [10];
 					else bowlers[currentBowler].frames[next_frame] = [10, 0];
-					reset_pins = true;
+					if (next_frame < 9) next_player = true;
 				} else bowlers[currentBowler].frames[next_frame] = [pins_amt];
 			}
 		} else if (frames[9].length === 1) {
 			bowlers[currentBowler].frames[9] = [frames[9][0], pins_amt];
-			if (frames[9][0] !== 10 && frames[9][0] + pins_amt !== 10) reset_pins = true;
+			if (frames[9][0] !== 10 && frames[9][0] + pins_amt !== 10) next_player = true;
 		} else if (frames[9].length === 2 && (frames[9][0] === 10 || frames[9][0] + frames[9][1] === 10)) {
 			bowlers[currentBowler].frames[9] = [frames[9][0], frames[9][1], pins_amt];
+			next_player = true;
+		}
+		if (next_player) {
 			reset_pins = true;
+			const bowler_names = Object.keys(bowlers);
+			const next_index = bowler_names.indexOf(currentBowler) + 1;
+			const back_to_start = next_index >= bowler_names.length;
+			if (back_to_start) currentFrame++;
+			currentBowler = bowler_names[back_to_start ? 0 : next_index];
 		}
 		if (reset_pins) pins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+	};
+
+	const randomPins = (amt?: number) => {
+		// Only for testing purposes
+		const frames = bowlers[currentBowler].frames;
+		const last_frame = frames.at(-1) ?? [];
+		if (isNaN(amt)) amt = Math.round(Math.random() * (10 - (last_frame.at(-1) ?? 0)));
+		else if (amt > pins.length) amt = pins.length;
+
+		let returned_pins: Pins = [];
+		const possible_pins: Pins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+		for (let i = 0; i < amt; i++) {
+			let random = 0;
+			while (returned_pins.includes(random)) {
+				random = possible_pins[Math.floor(Math.random() * possible_pins.length)];
+			}
+			returned_pins.push(random);
+		}
+		return returned_pins;
 	};
 
 	const keyPress = (event: KeyboardEvent) => {
 		if (event.key === "u") screenType = "user";
 		else if (event.key === "t") screenType = "tv";
-		else if (+event.key) {
-			const this_bowler = Object.keys(bowlers)[+event.key - 1];
-			if (this_bowler) currentBowler = Object.keys(bowlers)[+event.key - 1];
-		} else if (event.key === "b") {
-			const last_frame = bowlers[currentBowler].frames.at(-1) ?? [];
-			const p = Math.round(Math.random() * (10 - (last_frame.at(-1) ?? 0)));
-			bowlPins([...Array(p).keys()]);
-		} else if (event.key === "s") bowlPins([...Array(10).keys()]);
+		else if (event.key === "b") {
+			bowlPins(randomPins());
+		} else if (Number.isInteger(+event.key)) bowlPins(randomPins(+event.key));
+		else if (event.key === "/") bowlPins(pins);
+		else if (event.key === "i") {
+			if (interval) {
+				clearInterval(interval);
+				interval = null;
+			} else {
+				const bowler_names = Object.keys(bowlers);
+				const next_index = bowler_names.indexOf(currentBowler) - 1;
+				const back_to_start = next_index < 0;
+				const lastBowler = bowler_names[back_to_start ? bowler_names.length - 1 : next_index];
+				const ms_frame = bowlers[lastBowler].frames.at(-1) ?? [];
+
+				const part1 = ms_frame[0]?.toString() ?? "0";
+				const part2 = ms_frame[1]?.toString() ?? "5";
+				const ms = +(part1 + part2) * 100;
+				console.log(ms);
+				interval = setInterval(advanceGame, ms);
+			}
+		}
 	};
 
-	const advanceGame = () => {
-		const bowler_names = Object.keys(bowlers);
-		const frames = bowlers[currentBowler].frames;
-		let last_frame = frames.at(-1) ?? [];
-		const p = Math.round(Math.random() * (10 - (last_frame.at(-1) ?? 0)));
-		let pins_to_bowl: Pins = [];
-		const possible_pins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-		for (let i = 0; i < p; i++) {
-			let random = 0;
-			while (pins_to_bowl.includes(random)) {
-				random = possible_pins[Math.floor(Math.random() * possible_pins.length)];
-			}
-			pins_to_bowl.push(random);
-		}
-		bowlPins(pins_to_bowl);
-		last_frame = frames.at(-1) ?? [];
-		const mid_frame_done = frames.length < 10 && last_frame.length === 2;
-		const last_frame_done = frames.length === 10 && (last_frame.length === 3 || last_frame[0] + last_frame[1] < 10);
-		if (mid_frame_done || last_frame_done) {
-			const next_index = bowler_names.indexOf(currentBowler) + 1;
-			const back_to_start = next_index >= bowler_names.length;
-			if (back_to_start) currentFrame++;
-			if (last_frame_done && back_to_start) clearInterval(interval);
-			else currentBowler = bowler_names[back_to_start ? 0 : next_index];
-		}
-	};
-	let interval;
-	onMount(() => {
+	const resetGame = () => {
 		Object.keys(bowlers).forEach((bowler) => (bowlers[bowler] = { frames: [] }));
 		currentBowler = Object.keys(bowlers)[0];
 		currentFrame = 1;
-		interval = setInterval(advanceGame, 100);
-	});
+	};
+	const advanceGame = () => {
+		bowlPins(randomPins());
+		if (currentFrame === 11) clearInterval(interval);
+	};
+	let interval;
+	onMount(() => resetGame());
 	onDestroy(() => clearInterval(interval));
 </script>
 
